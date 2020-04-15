7497cd418f0c56dcd49b1d095573c96e57677d2b
flash <de la casa.com>
2020-04-15 07:54:43 -0300

    ruleta

--- eje1.py
+++ eje1.py
@@ -1,26 +1,50 @@
 import random
-nroVecesGanados=0
-cantTiros=0
-resultados=[]
+import matplotlib.pyplot as plt
+import numpy as np
+
+nroVecesGanados = 0
+cantTiros = 0
+resultados = []
+cantVeces = 20
+frecRelativas = []
+
 print('\tJUEGO DE RULETA\t ')
-rta=input('ingrese s para comenzar a jugar: ')
-while rta=='s':
-    nume=int(input('INGRESE UN NUMERO PARA JUGAR: '))
-    if (0<=nume <=36):
-            nroaleatorio=random.randrange( 00, 36)
-            cantTiros+=1
-            if (nume!=nroaleatorio):
-                print('Lo siento, cayó en el numero ' + str(nroaleatorio))
-                rta=input('DESEA SEGUIR JUGANDO?: ')
-            else:
-                 print(' ..¡¡ FELICITACIONES GANASTE !!..')
-                 rta=input('DESEA SEGUIR JUGANDO?: ')
-                 nroVecesGanados=+1
-            resultados.append(nroaleatorio)             
-    else:
-            print("el numero ingresado no esta en el rango vuelve a ingresar nro") 
-            
-print('cantidad de veces que ganaste:' + str (nroVecesGanados))   
-print('cantidad de tiros jugados:' + str (cantTiros))   
-print(resultados)      
-  
+while True:
+    nume = int(input('INGRESE UN NUMERO PARA JUGAR: (Del 0 al 36)'))#5
+    if nume < 36 or nume > 0:
+        break 
+
+for i in range(1,cantVeces + 1):
+    nroaleatorio=random.randrange( 00, 36) # 5
+    
+    if (nume == nroaleatorio):
+                
+        nroVecesGanados=+1 # 1
+         
+    frecRelativas.append(nroVecesGanados / i) #[0/1, 1 / 2, 1 / 3, 1 / 4, 1 / 5, 1 / 6, 1 / 7] 
+
+    resultados.append(nroaleatorio)             
+        
+#print('Cantidad de veces que ganaste: ' + str (nroVecesGanados))   
+
+#print(resultados)      
+
+## [5, 3, 4, 4]
+## Frecuencia relativa,  valor promedio, valor del desvio y valor de la varianza
+## [3 , 8 , 19, 3 ] 2 veces de las 4 veces que tiramos salio 3... fr(3) = 2/4
+
+#nroVecesGanados es la cant de veces que salio el numero
+
+## for f in frecRelativas:
+##     # Para cada frecuencia relativa la mostramos
+##     print("La frecuencia relativa es de " + str(f))
+
+altura = 1 / 37
+print(altura)
+plt.axhline(altura)
+plt.ylabel("Frecuencia")
+plt.xlabel("Numero de tiradas")
+plt.axis([ 0 , cantVeces , 0 , 1])
+#frecRelativas = [0.02, 0.03, 0.04]
+plt.plot(frecRelativas, "-r")# [1 , 2 , 3 , 4]
+plt.show()
