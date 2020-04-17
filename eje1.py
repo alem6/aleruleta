82bb42b5d93e6edee41beb3033e29197f6cdf2dd
alem6 <63481868+alem6@users.noreply.github.com>
2020-04-17 10:38:47 -0300

    altura

--- eje1.py
+++ eje1.py
@@ -5,24 +5,35 @@ import numpy as np
 nroVecesGanados = 0
 cantTiros = 0
 resultados = []
-cantVeces = 5000
+cantVeces = 50
 frecRelativas = []
+media = []
+desvio = []
+varianza = []
+valoresAcumulados = 0
+acumDesvio=0
+
 # Comienza el programa
 print('\tJUEGO DE RULETA\t ')
 while True:
-    nume = int(input('INGRESE UN NUMERO PARA JUGAR: (Del 0 al 36)'))#5
+    nume = int(input('INGRESE UN NUMERO PARA JUGAR: (Del 0 al 36): '))#5
     if nume < 36 or nume > 0:
         break 
 
 for i in range(1 , cantVeces + 1):
     nroaleatorio = random.randrange( 00, 37) # 5
+    valoresAcumulados += nroaleatorio
     
     if (nume == nroaleatorio):
         nroVecesGanados += 1 # 1
          
-    frecRelativas.append(nroVecesGanados / i) #[0/1, 1 / 2, 1 / 3, 1 / 4, 1 / 5, 1 / 6, 1 / 7] 
+    frecRelativas.append(nroVecesGanados / i)
+    media.append(valoresAcumulados/i)
+    resultados.append(nroaleatorio)
+    desvio.append(np.std(resultados))
+    varianza.append(np.var(resultados))
+    
 
-    resultados.append(nroaleatorio)             
 ## [5, 3, 4, 4]
 ## Frecuencia relativa,  valor promedio, valor del desvio y valor de la varianza
 ## [3 , 8 , 19, 3 ] 2 veces de las 4 veces que tiramos salio 3... fr(3) = 2/4
@@ -30,17 +41,52 @@ for i in range(1 , cantVeces + 1):
 ##     # Para cada frecuencia relativa la mostramos
 ##     print("La frecuencia relativa es de " + str(f))
 
+# (0,1,2,3,4,5,6,...,36)/37
 
-altura = 1 / 37
-#print(altura)
-plt.axhline(altura)
-plt.ylabel("Frecuencia")
-plt.xlabel("Numero de tiradas")
-plt.axis([ 0 , cantVeces , 0 , 1])
-#frecRelativas = [0.02, 0.03, 0.04]
-plt.plot(frecRelativas, "-r")# [1 , 2 , 3 , 4]
-#plt.show()
-try:
-    plt.savefig("graficosRuleta.pdf")
-except:
-    print("Ha ocurrido un error")
+if nroVecesGanados > 0:
+    altura = 1 / 37
+    #print(altura)
+    plt.axhline(altura)
+    plt.ylabel("FR(Frecuencia Relativa)")
+    plt.xlabel("Numero de tiradas")
+    plt.axis([ 0 , cantVeces , 0 , 1])
+    #frecRelativas = [0.02, 0.03, 0.04]
+    plt.plot(frecRelativas, "-r")# [1 , 2 , 3 , 4]
+    print(frecRelativas[-1])
+    #plt.savefig("frecuenciaFiguras.pdf")
+    plt.show()
+
+if nroVecesGanados > 0:
+    altura = np.median(resultados)
+    plt.axhline(altura)
+    plt.ylabel("VP(Valor Promedio de las tiradas")
+    plt.xlabel("Numero de tiradas")
+    plt.axis([ 0 , cantVeces , 0 , 200])
+    plt.plot(media, "-r")# [1 , 2 , 3 , 4]
+    print(media[-1])
+    plt.show()
+    #plt.savefig("promedioFiguras.pdf")
+
+if nroVecesGanados > 0:
+    altura = np.std(resultados)
+    plt.axhline(altura)
+    plt.ylabel("VD(Valor del Desvio)")
+    plt.xlabel("Numero de tiradas")
+    plt.axis([ 0 , cantVeces , 0 , 200])
+    plt.plot(desvio, "-r")# [1 , 2 , 3 , 4]
+    print(desvio[-1])
+    #plt.savefig("desvioFiguras.pdf")
+    plt.show()
+
+
+if nroVecesGanados > 0:
+    altura = np.var(resultados)
+    plt.axhline(altura)
+    plt.ylabel("VV(Valor de la Varianza)")
+    plt.xlabel("Numero de tiradas")
+    plt.axis([ 0 , cantVeces , 0 , 200])
+    plt.plot(varianza, "-r")# [1 , 2 , 3 , 4]
+    #plt.savefig("varianzaFiguras.pdf")
+    print(varianza[-1])
+    plt.show()
+
