5a67d57070bf53bf93f4eca46b2a7d0c7d766099
Alejandro Navarini <ale.navarini99@gmail.com>
2020-06-04 02:36:49 -0300

    Agregue 3 distribuciones

--- Tp_2_2/generadorAleatorio.py
+++ Tp_2_2/generadorAleatorio.py
@@ -15,8 +15,25 @@ def exp(ex):
 
 def gamma(k, a):
     tr = 1.0
-    for i in range(5):
+    for i in range(k):
         r = random.random()
         tr *= r
     x = -math.log(tr)
-    
+    return x
+
+def normal(ex ,stdx):
+    sum = 0
+    for i in range(12):
+        r = random.random()
+        sum += r
+    x = stdx * (sum - 6) + ex
+    return x
+
+def pascal(k,q):
+    tr = 1.0
+    qr = math.log10(q)
+    for i in range(k):
+        r = random.random()
+        tr *= r
+    nx = math.log10(tr) / qr
+    return nx    
